8417c6038a2b0991b0811bcea008b440fe3b2c70
Quojo_Steve <quojosteve@gmail.com>
2022-11-12 20:45:23 +0000

    final commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const carousel = document.querySelector(".carousel"),
+    firstImg = carousel.querySelectorAll("img")[0],
+    arrowIcons = document.querySelectorAll(".wrapper .next");
+
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, postionDiff;
+
+const showHideIcons = () => {
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14;
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60);
+    })
+});
+
+const autoSlide = () => {
+    if (carousel.scrollLeft == (carousel.scrollWidth - carousel.clientWidth)) return;
+
+    postionDiff = Math.abs(postionDiff);
+    let firstImgWidth = firstImg.clientWidth + 14;
+    let valDifference = firstImgWidth - postionDiff;
+
+    if (carousel.scrollLeft > prevScrollLeft) {//for scrolling right
+        return carousel.scrollLeft += postionDiff > firstImgWidth / 3 ? valDifference : -postionDiff;
+    }//for scrolling left
+    carousel.scrollLeft -= postionDiff > firstImgWidth / 3 ? valDifference : -postionDiff;
+}
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+ 
+const dragging = (e) => {
+    if (!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add("dragging");
+    isDragging = true;
+    postionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - postionDiff;
+    showHideIcons();
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+    if (!isDragging) return;
+    isDragging = false;
+    autoSlide()
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
